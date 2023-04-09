466073370e4129a9a1649995427b2f474ff722f2
Jin Wang <jin.wang@iress.com>
2023-04-09 16:51:48 +1000

    initializeApp support firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import {ctx} from "./context/Context";
 import {getMessage} from "common/lib/util/Util";
 
+admin.initializeApp();
+
 export const addGame = functions.https.onRequest(async (request, response) => {
   ctx.logMgr.info( "Adding game: ", request.body );
-  // await ctx.service.sitAndGoService.addGame( request.body as any );
+  await ctx.service.sitAndGoService.addGame( request.body as any );
   response.send("OK4: " + getMessage());
 });
