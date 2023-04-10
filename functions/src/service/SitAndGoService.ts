fbe949a7512f0eb639ee7537d81d339596df0bac
Jin Wang <jin.wang@iress.com>
2023-04-10 12:42:57 +1000

    game stats

--- functions/src/service/SitAndGoService.ts
+++ functions/src/service/SitAndGoService.ts
@@ -4,9 +4,13 @@ import CollectionReference = firestore.CollectionReference;
 import DocumentData = firestore.DocumentData;
 import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
 import Timestamp = firestore.Timestamp;
+import SitAndGoGameStats from "common/lib/model/sitAndGo/SitAndGoGameStats";
 
 export const createGame = async ( game: SitAndGoGame ): Promise<void> => {
     await getCollection().add( game );
+    const lastGameStats = await getLastGameStats( game.buyIn );
+    const gameStats = fromStats( game, lastGameStats );
+    await getStatsCollection().add( gameStats );
 };
 
 export const getLastGame = async (): Promise<SitAndGoGame> => {
@@ -15,6 +19,63 @@ export const getLastGame = async (): Promise<SitAndGoGame> => {
         .limit(1)
         .get();
     return fromDocument( query.docs[0] );
+};
+
+export const getLastGameStats = async ( buyIn: number ): Promise<SitAndGoGameStats | undefined> => {
+    const query = await getStatsCollection()
+        .where( "buyIn", "==", buyIn )
+        .orderBy("createDateTime", "desc")
+        .limit(1)
+        .get();
+    return fromStatsDocument( query.docs[0] );
+};
+
+const fromStatsDocument = ( doc: QueryDocumentSnapshot ): SitAndGoGameStats | undefined => {
+    if ( !doc ) {
+        return;
+    }
+    const docData = doc.data();
+    return {
+        ...docData,
+        id: doc.id,
+        dateTime: ( docData.dateTime as Timestamp ).toDate(),
+        createDateTime: ( docData.createDateTime as Timestamp ).toDate()
+    } as SitAndGoGameStats;
+};
+
+const fromStats = ( game: SitAndGoGame, stats?: SitAndGoGameStats ): SitAndGoGameStats => {
+    if ( !stats ) {
+        return {
+            createDateTime: new Date(),
+            dateTime: game.dateTime,
+            buyIn: game.buyIn,
+            buyInData: {
+                games: 1,
+                winGames: game.result === "win" ? 1 : 0,
+                winRate: game.result === "win" ? 1 : 0
+            },
+            totalData: {
+                games: 1,
+                winGames: game.result === "win" ? 1 : 0,
+                winRate: game.result === "win" ? 1 : 0
+            }
+        };
+    }
+    return {
+        createDateTime: new Date(),
+        dateTime: game.dateTime,
+        buyIn: game.buyIn,
+        buyInData: {
+            games: stats.buyInData.games + 1,
+            winGames: game.result === "win" ? stats.buyInData.winGames + 1 : stats.buyInData.winGames,
+            winRate: stats.buyInData.winGames / stats.buyInData.games
+        },
+        totalData: {
+            games: stats.totalData.games + 1,
+            winGames: stats.totalData.winGames / stats.totalData.games,
+            winRate: stats.totalData.winGames / stats.totalData.games
+        }
+    };
 };
 
 export const fromDocument = ( doc: QueryDocumentSnapshot ): SitAndGoGame => {
@@ -38,4 +99,8 @@ export const fromRequestBody = ( requestBody: any ): SitAndGoGame => {
 
 const getCollection = (): CollectionReference<DocumentData> => {
     return firestore().collection( "SitAndGoGame" );
+};
+
+const getStatsCollection = (): CollectionReference<DocumentData> => {
+    return firestore().collection( "SitAndGoGameStats" );
 };
