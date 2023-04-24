e8bc8e87fe000d9a0dc52303ddcb45d09104b6fd
Jin Wang <jin.wang@iress.com>
2023-04-24 22:21:24 +1000

    more customs

--- frontend/src/component/BalanceLineChart.tsx
+++ frontend/src/component/BalanceLineChart.tsx
@@ -6,28 +6,31 @@ import "./BalanceLineChart.css";
 
 export const BalanceLineChart = ( props: BalanceLineChartProps ) => {
 
-    const getCustomXAxisTickFormatter = () => {
-        let previousYear = 0;
-        let previousMonth = 0;
-        return ( value: any ) => {
-            const date = new Date( value );
-            const dateYear = date.getFullYear();
-            const dateMonth = date.getMonth() + 1;
-            if ( previousYear !== dateYear || previousMonth !== dateMonth ) {
-                previousYear = dateYear;
-                previousMonth = dateMonth;
-                return `${dateYear}.${dateMonth}`;
-            }
-            return "               ";
-        };
+    const customRenderXAxisTick = ( tickProps: any ) => {
+        if ( tickProps.index == 0 ) {
+            return <text x={tickProps.x + 100} y={tickProps.y + 35} fill="#666" textAnchor="middle">2021.12.12 (BuyIn $0.01M and $0.025M)</text>;
+        } else if ( tickProps.index == 1 ) {
+            return <></>;
+        }
+        return <text x={tickProps.x} y={tickProps.y + 15} fill="#666" textAnchor="middle">{tickProps.payload.value}</text>;
     };
 
     const customYAxisTickFormatter = ( value: any ) => {
-        return `${value}M`;
+        return `$${value}M`;
     };
 
-    const customTooltipFormatter = ( value: any, name: any, props: any ): [string, string] => {
-        return [`${value}M`, "Balance"];
+    const customRenderTooltip = ( tooltipProps: any ) => {
+        const active = tooltipProps.active;
+        if ( !active ) {
+            return <></>;
+        }
+        const item = tooltipProps.payload[0].payload as BalanceLineChartDataItem;
+        return (
+            <div className="mdp-ChartTooltip">
+                <p className="mdp-ChartTooltipItem">{`Date : ${item.date}`}</p>
+                <p className="mdp-ChartTooltipItem">{`Balance : $${item.balance}M`}</p>
+            </div>
+        );
     };
 
     const customLegendFormatter = ( value: any ) => {
@@ -42,9 +45,9 @@ export const BalanceLineChart = ( props: BalanceLineChartProps ) => {
             <ResponsiveContainer width="100%" height="100%">
                 <LineChart width={500} height={300} data={props.data.items}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" tickFormatter={getCustomXAxisTickFormatter()} />
+                    <XAxis dataKey="milestone" interval={0} ticks={props.data.milestones} tick={customRenderXAxisTick} />
                     <YAxis tickFormatter={customYAxisTickFormatter} />
-                    <Tooltip formatter={customTooltipFormatter} />
+                    <Tooltip content={customRenderTooltip} />
                     <Legend formatter={customLegendFormatter} />
                     <Line type="monotone" dataKey="balance" stroke="#8884d8" activeDot={{ r: 8 }} />
                 </LineChart>
@@ -59,20 +62,43 @@ export type BalanceLineChartProps = {
 
 export type BalanceLineChartData = {
     items: BalanceLineChartDataItem[];
+    milestones: string[];
 };
 
 export type BalanceLineChartDataItem = {
     index: number;
     date: string;
+    milestone: string;
+    buyIn: number;
     balance: number;
 };
 
 export const convertToBalanceLineChartData = ( gameData: SitAndGoData ): BalanceLineChartData => {
-    return {
-        items: gameData.games.map( ( item, index ) => ( {
+    const milestones: string[] = [];
+    let currentBuyIn: number;
+    const items = gameData.games.map( ( item, index ) => {
+        const buyIn = Number( ( item.data.buyIn / (1000 * 1000) ).toFixed( 3 ) );
+        let milestone = "";
+        if ( currentBuyIn !== buyIn ) {
+            currentBuyIn = buyIn;
+            const date = new Date( item.data.dateTime );
+            const dateYear = date.getFullYear();
+            const dateMonth = date.getMonth() + 1;
+            const dateDay = date.getDate();
+            milestone = `${dateYear}.${dateMonth}.${dateDay} (BuyIn $${currentBuyIn}M)`;
+            milestones.push( milestone );
+        }
+        return {
             index: index,
             date: item.data.dateTime,
+            milestone,
+            buyIn,
             balance: Number( ( item.totalStats.balance / (1000 * 1000) ).toFixed( 1 ) )
-        } ) )
+        };
+    } );
+
+    return {
+        items,
+        milestones
     };
 };
